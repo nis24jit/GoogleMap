ccfb4db21f462ad1782d60ddfe504876320f64fd
Nishanth <>
2020-04-10 21:45:32 +0200

    Initial Commit

---
+++ maps/src/Company.ts
@@ -0,0 +1,24 @@
+import faker from 'faker';
+import { Mappable } from './CustomMap';
+export class Company implements Mappable {
+  companyName: string;
+  catchPhrase: string;
+  location: {
+    lat: number;
+    lng: number;
+  };
+  color: string = 'green';
+
+  constructor() {
+    this.companyName = faker.company.companyName();
+    this.catchPhrase = faker.company.catchPhrase();
+    this.location = {
+      lat: parseFloat(faker.address.latitude()),
+      lng: parseFloat(faker.address.longitude()),
+    };
+  }
+
+  markerContent(): string {
+    return `<h1>Company name ${this.companyName}</h1>`;
+  }
+}
